28681b5ef9c7ee735a50f9bdfa58e678425ea649
dkamundi <davekamundi@gmail.com>
2023-06-13 19:04:29 -0700

    4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// class defining a rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      			this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      let rectView = '';
+      for (let index = 0; index < this.width; index++) {
+        rectView += 'X';
+      }
+      console.log(rectView);
+    }
+  }
+};
