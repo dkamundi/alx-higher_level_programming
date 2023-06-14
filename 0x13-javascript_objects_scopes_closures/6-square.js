5b300d99f21c9f073bf796d235e48eb559ea3f81
dkamundi <davekamundi@gmail.com>
2023-06-13 19:26:50 -0700

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// class defining a square
+// inheriting from a rectangle
+
+const square = require('./5-square');
+module.exports = class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    let squareString = '';
+
+    for (let i = 0; i < this.width; i++) {
+      squareString += c.repeat(this.width) + '\n';
+    }
+
+    console.log(squareString);
+  }
+};
