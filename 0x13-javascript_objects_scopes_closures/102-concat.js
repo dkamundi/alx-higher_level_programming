8b8f84f7b2bae2da3ea6a79275ceb1aa4a6f1276
dkamundi <davekamundi@gmail.com>
2023-06-14 11:51:34 -0700

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,37 +1,8 @@
 #!/usr/bin/node
-
 const fs = require('fs');
+// read the contents of fileA and fileB
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
+const result = fileA + fileB;
 
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-// Read the content of the first source file
-fs.readFile(sourceFile1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${sourceFile1}:`, err);
-    return;
-  }
-
-  // Read the content of the second source file
-  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(`Error reading ${sourceFile2}:`, err);
-      return;
-    }
-
-    // Concatenate the contents of both files
-    const concatenatedContent = data1 + data2;
-
-    // Write the concatenated content to the destination file
-    fs.writeFile(destinationFile, concatenatedContent, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing to ${destinationFile}:`, err);
-        return;
-      }
-
-      console.log(`Concatenation successful. Result saved to ${destinationFile}`);
-    });
-  });
-});
-
+fs.writeFileSync(process.argv[4], result);
