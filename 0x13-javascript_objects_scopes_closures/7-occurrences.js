98097ec6047c6fd02241a9bd6f680f2f49d911da
dkamundi <davekamundi@gmail.com>
2023-06-14 01:05:50 -0700

    7. Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nb0ccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
 	let count = 0;
 	for (let i = 0; i < list.length; i++) {
 		if (list[i] === searchElement) {
