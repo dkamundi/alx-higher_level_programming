045e8e16a018169c7644b6dc4c69a71a9e96826e
dkamundi <davekamundi@gmail.com>
2023-06-13 18:24:54 -0700

    9. Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('Invalid input. Please provide two integers.');
+} else {
+  const result = add(arg1, arg2);
+  console.log(result);
+}
