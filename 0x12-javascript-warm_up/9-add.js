08dde22e302775fac75091395a587bfaa3f2058a
dkamundi <davekamundi@gmail.com>
2023-06-14 11:58:07 -0700

    9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
 if (isNaN(arg1) || isNaN(arg2)) {
-  console.log('Invalid input. Please provide two integers.');
+  console.log('NaN')
 } else {
   const result = add(arg1, arg2);
   console.log(result);
