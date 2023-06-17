a93406a550bf04cf19139e137537b32ce7d3dc2b
dkamundi <davekamundi@gmail.com>
2023-06-17 16:22:06 -0700

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- full creation tableand information
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score);
-VALUES
+INSERT INTO second_table (id, name, score) VALUES
 	("1", "John", "10"),
 	("2", "Alex", "3"),
 	("3", "Bob", "14"),
