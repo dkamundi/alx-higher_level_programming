e046789e44d91aa069cc9191a52312ef88802605
dkamundi <davekamundi@gmail.com>
2023-06-17 16:49:40 -0700

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 import MySQLdb
 
-#Get coommand-line arguements
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+# Get command-line arguments
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-#Connect to MySQL server
-db = MySQLdb.connect(host="localhost",port=3306, user=username, passwd=password, db=database)
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-#create a cursor object to interract with the database
-cursor = db.cursor()
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-#Execue the SQL query to retrieve all states
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Execute the SQL query to retrieve all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-#Fetch all rows from the result set
-rows = cursor.fetchall()
+    # Fetch all the rows from the result set
+    rows = cursor.fetchall()
 
-#Print the states
-for row in rows:
-    print(row[1])
+    # Print the states
+    for row in rows:
+        print(row)
 
-#Close the cursor and database connection
-cursor.close()
-db.close()
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
 
