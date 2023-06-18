de922bc7f0ad87b9bd0638b21f6c606092667a53
dkamundi <davekamundi@gmail.com>
2023-06-18 16:59:34 -0700

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,43 +1,21 @@
 #!/usr/bin/python3
+
 """
-Script that displays all values in the states table of hbtn_0e_0_usa where
-name matches the argument
+lists all states with a given name
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    # Script arguments
-    mysql_user = argv[1]
-    mysql_password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=db_name
-    )
-
-    # Create cursor to execute queries
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-
-    # Select states matching the provided name
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all rows
-    states = cursor.fetchall()
-
-    # Print the states
-    for state in states:
-        print(state)
-
-    # Close cursor and database connections
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     db.close()
-
