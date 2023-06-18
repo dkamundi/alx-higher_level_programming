779ac7d1690e51ce485525b3630487cb5cde67e8
dkamundi <davekamundi@gmail.com>
2023-06-18 00:33:49 -0700

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,51 @@
 #!/usr/bin/python3
-import sys
+"""
+This module defines a script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument, using
+parameterized queries to prevent SQL injection.
+"""
+
 import MySQLdb
+import sys
 
-# Get command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
 
-# Connect to the MySQL server
-db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+def safe_select_states(username, password, db_name, state_name):
+    """
+    This function takes in four arguments and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument, using parameterized
+    queries to prevent SQL injection.
+    """
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    # Get a cursor
+    cursor = db.cursor()
 
-# Create the parameterized SQL query
-sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    # Define the query with a parameter placeholder
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
 
-# Execute the SQL query with the state name as a parameter
-cursor.execute(sql_query, (state_name,))
+    # Execute the query with the parameter value
+    cursor.execute(query, (state_name,))
 
-# Fetch all the rows from the result set
-rows = cursor.fetchall()
+    # Fetch all the rows as a list of tuples
+    rows = cursor.fetchall()
 
-# Print the states
-for row in rows:
-    print(row)
+    # Print the rows
+    for row in rows:
+        print(row)
 
-# Close the cursor and database connection
-cursor.close()
-db.close()
+    # Close cursor and database
+    cursor.close()
+    db.close()
 
+
+if __name__ == "__main__":
+    # Call the safe_select_states() function with the provided arguments
+    safe_select_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
