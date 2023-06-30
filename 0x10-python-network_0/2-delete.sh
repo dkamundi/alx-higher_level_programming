d245472b677b73ac01917c6b284de20a71b409f3
dkamundi <davekamundi@gmail.com>
2023-06-30 08:15:17 -0700

    2. cURL Method

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script: delete_request.sh
+# Sends a DELETE request to a URL and displays the body of the response
+
+URL=$1
+
+response=$(curl -s -X DELETE "$URL")
+
+echo "$response"
+
