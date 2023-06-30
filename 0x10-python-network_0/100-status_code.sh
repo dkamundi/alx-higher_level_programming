3c22b1446a679f039854dbb54b9933ba74262f7b
dkamundi <davekamundi@gmail.com>
2023-06-30 09:24:53 -0700

    7. Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Bash script to send a request to a URL and display only the status code
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url=$1
-
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-echo "Status code: $status_code"
-
+# sends a request to a URL, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
