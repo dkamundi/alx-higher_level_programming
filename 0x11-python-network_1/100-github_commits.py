f55417e04ca011060ccae6c90efaf5b4e80c6d62
dkamundi <davekamundi@gmail.com>
2023-06-30 09:18:36 -0700

    10. Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module Description: This script uses the GitHub API to retrieve and print the 10 most recent commits of a given repository by a specific user.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error accessing GitHub API:", response.status_code)
+
