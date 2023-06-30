3638319bb116c9106a73a0cbece1a2b8022cf59c
dkamundi <davekamundi@gmail.com>
2023-06-30 08:23:52 -0700

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Script: 3-methods.sh
-# Displays all HTTP methods the server will accept for a given URL
-
-URL=$1
-
-response=$(curl -sI -X OPTIONS "$URL" | grep "Allow:" | sed 's/Allow: //')
-
-echo "$response"
-
+# display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
