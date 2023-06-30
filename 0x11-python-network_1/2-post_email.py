7af23fdbbfbb84754bc84c1323fa3dcb59082986
dkamundi <davekamundi@gmail.com>
2023-06-30 08:50:00 -0700

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module Description: This script sends a POST request to a given URL with an email parameter and displays the response body.
+"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    request = urllib.request.Request(url, data=data, method="POST")
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode("utf-8")
+
+    print(body)
+
