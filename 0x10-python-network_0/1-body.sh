5683788e67a1f774a77db389d2cdd94020b2ce4a
dkamundi <davekamundi@gmail.com>
2023-06-30 08:10:59 -0700

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,11 +2,4 @@
 # Script: Get request body
 # Sends a GET request to a URL and displays the body of the response
 
-URL=$1
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-if [ "$response" -eq 200 ]; then
-    curl -s "$URL"
-fi
-
+curl -sI "$1" | grep Content-Length | cut -d " " -f 2
